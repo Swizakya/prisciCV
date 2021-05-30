cb9e8986f3234fa9c1a95e91c94ad57da40c6a43
Swizakya <66135666+Swizakya@users.noreply.github.com>
2021-05-30 11:06:49 -0300

    my first

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+//     target: '#cvNavbar'
+//   })
+
+// var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
+// dataSpyList.forEach(function (dataSpyEl) {
+//   bootstrap.ScrollSpy.getInstance(dataSpyEl)
+//     .refresh()
+// })
+
